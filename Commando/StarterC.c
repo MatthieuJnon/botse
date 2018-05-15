3f5cbe6cfd1615bd6c11e3d471210dd60d565c9d
Matthieu Joannon <mjoannon@methys.com>
2018-05-15 10:53:53 +0200

    method names

---
+++ Commando/StarterC.c
@@ -0,0 +1,17 @@
+#include "StarterC.h"
+#include "Pilot.h"
+#include "AdminUI.h"
+#include "DispatcherCommando.h"
+#include "Robot.h"
+#include "Logger.h"
+#include "ProxyRemoteUI.h"
+#include "PostmanCommando.h"
+
+
+extern void start(void) {
+
+}
+
+extern void stop(void) {
+
+}
