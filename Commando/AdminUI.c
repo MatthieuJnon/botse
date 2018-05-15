456af96c0719b2559f2abce328f74d94841543b3
Piheros <34651188+Piheros@users.noreply.github.com>
2018-05-15 17:49:50 +0200

    Update AdminUI.c

--- Commando/AdminUI.c
+++ Commando/AdminUI.c
@@ -15,12 +15,10 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <errno.h>
-
 #include "AdminUI.h"
-#include "ProxyPilot.c"
 
-// PARAMETRE MAE
-#define NAME_MQ_BOX  "/bal_AdminUI"
+// PARAMAMETRE MAE
+#define NAME_MQ_BOX  "/bal_RemoteUI"
 #define MQ_MSG_COUNT 10
 #define MQ_MSG_SIZE 256
 
@@ -37,7 +35,7 @@ static const char * State_getName(int i)
     return State_Name[i];
 }
 
-#define ACTION_GENERATION S(A_NOP) S(A_SET_DIR) S(A_DISPLAY_LOG) S(A_RETURN) S(A_CLEAR) S(A_STOP) S(A_REFRESH)
+#define ACTION_GENERATION S(A_NOP) S(A_SET_DIR) S(A_SET_IP) S(A_DISPLAY_LOG) S(A_RETURN) S(A_CLEAR) S(A_STOP) S(A_REFRESH)
 #define S(x) x,
 typedef enum {ACTION_GENERATION ACTION_NB}  Action;
 #undef S
@@ -50,8 +48,7 @@ static const char * Action_getName(int i)
     return Action_Name[i];
 }
 
-
-#define EVENT_GENERATION S(E_SET_DIR) S(E_REFRESH) S(E_CLEAR) S(E_ASK_LOG) S(E_RETURN) S(E_STOP)
+#define EVENT_GENERATION S(E_SET_DIR) S(E_SET_IP) S(E_REFRESH) S(E_CLEAR) S(E_ASK_LOG) S(E_RETURN) S(E_STOP)
 #define S(x) x,
 typedef enum {EVENT_GENERATION EVENT_NB}  Event;
 #undef S
@@ -75,14 +72,13 @@ typedef struct
 	Event event;
 } MqMsg;
 
-// Attributs de AdminUI (singleton)
 static State MyState;
 static pthread_t MyThread;
 
-/* MAE de AdminUI */
 static Transition MySm [STATE_NB][EVENT_NB] =
 {
 	    [S_MAIN_SCREEN][E_SET_DIR] = {S_MAIN_SCREEN, A_SET_DIR},
+	    [S_MAIN_SCREEN][E_SET_IP]  = {S_MAIN_SCREEN, A_SET_IP},
 	    [S_MAIN_SCREEN][E_ASK_LOG] = {S_LOG_CREEN,   A_DISPLAY_LOG},
 	    [S_MAIN_SCREEN][E_STOP]    = {S_DEATH,       A_STOP},
 
@@ -99,7 +95,9 @@ typedef union
 } MqMsgAdapter;
 
 // VARIABLES
+static uint8_t myIp;
 static Direction myDir;
+static VelocityVector myVel;
 static Event myEvents[];
 static int currentEventNumber;
 static int previousEventNumber;
@@ -113,24 +111,47 @@ extern void AdminUI_start(void)
 {
 	previousEventNumber = 0;
 	AdminUI_new ();
+	KeyBoard_Start();
 }
 
 extern void AdminUI_stop(void)
 {
-
 	MqMsg msg = {.event = E_STOP};
-	TRACE("[AdminUI] demande d'arret de la lampe  \n");
+	TRACE("[AdminUI] demande d'arret de AdminUI \n");
 	AdminUI_mqSend (msg);
 
 	pthread_join(MyThread, NULL);
 
 	AdminUI_free();
+	KeyBoard_Stop();
+}
 
+extern void AdminUI_backToMainScreen(void){
+	MqMsg msg =  {.event = E_RETURN};
+	TRACE("[AdminUI] retour a l'ecran principal\n");
+	AdminUI_mqSend (msg);
+}
+
+extern void AdminUI_ask4log(void){
+	MqMsg msg =  {.event = E_ASK_LOG};
+	TRACE("[RemoteUI] demande  de Log\n");
+	AdminUI_mqSend (msg);
+}
+
+extern void AdminUI_clear(void){
+	MqMsg msg =  {.event = E_CLEAR};
+	TRACE("[AdminUI] clear logs\n");
+	AdminUI_mqSend (msg);
 }
 
 extern void AdminUI_setDir(Direction dir)
 {
 	myDir = dir;
+	myVel.power = (myDir==FORWARD)? 20 : -20;
+	myVel.dir = myDir;
+	MqMsg msg =  {.event = E_SET_DIR};
+	TRACE("[AdminUI] demande de changement du dir\n");
+	AdminUI_mqSend (msg);
 }
 
 extern void AdminUI_validate(void)
@@ -140,7 +161,7 @@ extern void AdminUI_validate(void)
 
 extern void AdminUI_toggleEmergencyStop(void)
 {
-	//ProxyPilot.toggleEmergencyStop();
+
 }
 
 extern void AdminUI_setEvents (Event events[])
@@ -159,10 +180,13 @@ static AdminUI_displayScreen(IdAdminScreen idAdminScreen)
 {
 	switch(idAdminScreen){
 		case SCREEN_MAIN:
-			printf "-----------------------Screen MAIN-------------------------------";
+			printf("\n-----------------------Screen Main--------------------------------\n");
 			break;
 		case SCREEN_LOG:
-			printf "-----------------------Screen LOG--------------------------------";
+			printf("\n-----------------------Screen lOG  --------------------------------\n");
+			break;
+		case SCREEN_DEATH:
+			printf("\n--------------------- Shutting Down  ------------------------------\n");
 			break;
 		default:
 			break;
@@ -171,7 +195,7 @@ static AdminUI_displayScreen(IdAdminScreen idAdminScreen)
 
 static AdminUI_displayEvents(Event event[])
 {
-	//TODO
+
 }
 
 MqMsg AdminUI_mqReceive ()
@@ -194,7 +218,6 @@ static void AdminUI_mqSend (MqMsg aMsg)
 	MqMsgAdapter msg;
 	mqd_t mq;
 	msg.data = aMsg;
-	/* envoi de l'ordre à travers la BAL */
 	mq = mq_open (NAME_MQ_BOX, O_WRONLY);
 	STOP_ON_ERROR(mq == -1);
 	check = mq_send (mq, msg.buffer, sizeof (msg.buffer), 0);
@@ -211,26 +234,25 @@ extern void AdminUI_new ()
 	mqd_t mq;
     TRACE("[AdminUI] Construction \n");
 
-	/* Create BAL */
+	/* creation de la BAL */
 	mqa.mq_maxmsg = MQ_MSG_COUNT;
 	mqa.mq_msgsize = MQ_MSG_SIZE;
 	mqa.mq_flags = 0;
-
-	check = mq_unlink (NAME_MQ_BOX);
+    check = mq_unlink (NAME_MQ_BOX);
 	mq = mq_open (NAME_MQ_BOX, O_CREAT, 0777, &mqa);
 	STOP_ON_ERROR(mq == -1);
 
 	check = mq_close (mq);
 	STOP_ON_ERROR(check != 0);
  	MyState = S_MAIN_SCREEN;
-	check = pthread_create (&MyThread, NULL, AdminUI_run, NULL);
+	check = pthread_create (&MyThread, NULL, RemoteUI_run, NULL);
 	STOP_ON_ERROR(check != 0);
 }
 
-extern void RemoteUI_free ()
+extern void AdminUI_free ()
 {
 	int check;
-	/* Destruction BAL */
+	/* destruction de la BAL */
 	check = mq_unlink (NAME_MQ_BOX);
 	STOP_ON_ERROR(check != 0);
 	TRACE("[AdminUI] Destruction \n");
@@ -241,12 +263,12 @@ static void AdminUI_performAction (Action anAction)
 	switch (anAction)
 	{
     case A_SET_DIR:
-        AdminUI_setDir();
+    	AdminUI_displayScreen(SCREEN_MAIN);
         break;
 
     case A_DISPLAY_LOG:
-    	displayScreen(SCREEN_LOG);
-    	AdminUI_displayEvents(myEvents);
+    	AdminUI_displayScreen(SCREEN_LOG);
+//      AdminUI_displayEvents(myEvents); ??
         break;
 
     case A_RETURN:
@@ -254,46 +276,50 @@ static void AdminUI_performAction (Action anAction)
     	break;
 
     case A_CLEAR:
+//    	AdminUI_clear ();
     	AdminUI_displayScreen(SCREEN_LOG);
         break;
 
     case A_STOP:
-    	AdminUI_stop();
-        break;
+    	AdminUI_displayScreen(SCREEN_DEATH);
+    	break;
 
     case A_REFRESH:
     	currentEventNumber=ProxyLogger_askEventsCount();
     	myEvents=getEvents(previousEventNumber, currentEventNumber);
     	previousEventNumber = currentEventNumber;
-    	AdminUI_displayEvents(myEvents);
+//    	AdminUI_displayEvents(myEvents); ??
+    	AdminUI_displayScreen(SCREEN_LOG);
     	break;
 
     default:
-        TRACE("[AdminUI] Action inconnue, problème dans la MAE \n ");
+        TRACE("[AdminUI] Action inconnue, pb ds la MAE \n ");
         break;
 	}
 }
 
-static void* AdminUI_run ()
+
+static void* RemoteUI_run ()
 {
 	MqMsg msg;
 	Action act;
 
 	while (MyState != S_DEATH)
 	{
-		msg = AdminUI_mqReceive ();
-		if (MySm[MyState][msg.event].destinationState == S_FORGET)
+		msg = Lampe_mqReceive ();
+		if (MySm[MyState][msg.event].destinationState == S_FORGET) // aucun état de destination, on ne fait rien
 		{
-			TRACE("[AdminUI] MAE, perte evenement %s  \n", Event_getName(msg.event));
+            // on trace juste l'évènement détruit
+			TRACE("[RemoteUI] MAE, perte evenement %s  \n", Event_getName(msg.event));
 		}
-		else
+		else /* on tire la transition */
 		{
-			TRACE("[AdminUI] MAE, traitement evenement %s \n",  Event_getName(msg.event));
+			TRACE("[RemoteUI] MAE, traitement evenement %s \n",  Event_getName(msg.event));
 			act = MySm[MyState][msg.event].action;
-			TRACE("[AdminUI] MAE, traitement action %s \n", Action_getName(act));
-			AdminUI_performAction (act);
+			TRACE("[RemoteUI] MAE, traitement action %s \n", Action_getName(act));
+			RemoteUI_performAction (act);
 			MyState = MySm[MyState][msg.event].destinationState;
-            TRACE("[AdminUI] MAE, va dans etat %s \n", State_getName(MyState));
+            TRACE("[RemoteUI] MAE, va dans etat %s \n", State_getName(MyState));
 		}
 	}
 	return (0);
