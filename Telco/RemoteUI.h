3b4f08d4b204171685c81dbb3beac96d570a222e
Thomas EHling <thomas.ehling@reseau.eseo.fr>
2018-05-15 17:08:14 +0200

    maj remoteUI

--- Telco/RemoteUI.h
+++ Telco/RemoteUI.h
@@ -5,14 +5,32 @@ typedef enum {
 	SCREEN_FORGET,
     SCREEN_MAIN,
     SCREEN_LOG,
-	SCRREN_DEATH,
+	SCREEN_DEATH,
 	NB_SCREEN
 } IdScreen;
+
+typedef enum {LEFT=0, RIGHT, FORWARD, BACKWARD} Direction;
+
+typedef struct
+{
+    Direction dir;
+    int power;
+} VelocityVector;
+
+typedef struct
+{
+    int speed;
+    int collision;
+    float luminosity;
+} PilotState;
 
 extern void RemoteUI_start(void);
 extern void RemoteUI_stop(void);
 extern void RemoteUI_setIP(int ip);
 extern void RemoteUI_setDir(Direction dir);
+extern void RemoteUI_backToMainScreen(void);
+extern void RemoteUI_ask4log(void);
+extern void RemoteUI_clear(void);
 extern void RemoteUI_validate(void);
 extern void RemoteUI_toggleEmergencyStop(void);
 extern void RemoteUI_setPiloteState (PilotState state);
