6d0eb21af2a1cdad2fc8f72db817ad60d7beb4c2
Matthieu Joannon <mjoannon@methys.com>
2018-05-15 11:15:42 +0200

    started and stopped everything

--- Commando/StarterC.c
+++ Commando/StarterC.c
@@ -9,9 +9,15 @@
 
 
 extern void start(void) {
-
+    adminui_start();
+    Pilot_start();
+    Logger_start();
+    DC_start();
 }
 
 extern void stop(void) {
-
+    Logger_stop();
+    Pilot_stop();
+    adminui_stop();
+    DC_stop();
 }
